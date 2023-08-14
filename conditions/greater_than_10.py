dc2a270ec7bc0a5f3e0aa5cc931ca21136a4efac
raymondnes <raynelreign@gmail.com>
2023-08-14 17:31:06 +0300

    greater than 10

---
+++ conditions/greater_than_10.py
@@ -0,0 +1,7 @@
+# For example, let's write a program that asks the user to enter a number, and checks if it’s greater than 10:
+
+number = int(input("Enter a number: "))
+if number > 10:
+    print(f"{number} is greater than 10.")
+
+print("End of program.")
