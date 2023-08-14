314ba5716f1f920dc8c4962f99c67061cff258e6
raymondnes <raynelreign@gmail.com>
2023-08-14 17:29:37 +0300

    conditions

---
+++ conditions/code_challenges/check_name.py
@@ -0,0 +1,7 @@
+# A Buggy Program
+# The following program is supposed to check if ship_name is equal to “Titanic”, but it contains a bug. Can you fix it?
+
+ship_name = input("Enter a ship name: ")
+
+if ship_name == "Titanic":
+    print("Hello Titanic!")
---
+++ conditions/code_challenges/equality.py
@@ -0,0 +1,8 @@
+# Write a program that prints “Exactly!” if number is exactly 20:
+
+number = int(input("Enter a number: "))
+if number == 20:
+    print("Exactly!")
+else:
+    pass
+    
